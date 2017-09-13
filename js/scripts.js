4bbb69c5c263ca05a480c7d05fb3e316e48863a8
Saneyee Sarkar <saneyeesarkar@gmail.com>
2017-09-12 19:30:50 -0700

    introduced buttons and back ground color n text color

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+ $("button#green").click(function() {
+   $("body").removeClass();
+   $("body").addClass("back-greencolor");
+});
+  $("button#blue").click(function() {
+    $("body").removeClass();
+    $("body").addClass("back-bluecolor");
+
+
+});
+
+
+});
